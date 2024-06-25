9d309320e115826e9126fef4972bd2b74ba392bb
Steven Hertz <hertzsteven@icloud.com>
2024-06-25 18:10:10 -0400

    2 ways

--- UnderstandBackgroundOverlayzstack/ContentView.swift
+++ UnderstandBackgroundOverlayzstack/ContentView.swift
@@ -9,12 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+      VStack(spacing:32) {
+        Image(systemName: "globe")
+          .imageScale(.large)
+          .foregroundStyle(.tint)
+        Text("Hello, world!")
+          .foregroundStyle(.white)
+          .padding()
+          .background(RoundedRectangle(cornerRadius: 8))
+          //            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
+        
+        ZStack(alignment: .center) {
+             RoundedRectangle(cornerRadius: 10)
+                 .fill(Color.blue)
+                 .frame(height: 50) // Adjust the frame height as needed
+             
+             Text("Hello, world!")
+                 .foregroundColor(.white)
+         }
+         .padding() // Optional: adds space around the ZStack
+
+        
+      }
         .padding()
     }
 }
