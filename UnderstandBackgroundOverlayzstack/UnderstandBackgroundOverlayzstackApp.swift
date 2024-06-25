f088eaa3549962a600ab8032359c2907d12a8a6b
Steven Hertz <hertzsteven@icloud.com>
2024-06-25 17:05:46 -0400

    Initial Commit

---
+++ UnderstandBackgroundOverlayzstack/UnderstandBackgroundOverlayzstackApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderstandBackgroundOverlayzstackApp.swift
+//  UnderstandBackgroundOverlayzstack
+//
+//  Created by Steven Hertz on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct UnderstandBackgroundOverlayzstackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
